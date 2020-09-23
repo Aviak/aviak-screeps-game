2b829bf091cb9e08c2514004f20542d7828ad43e
Aviak <aviack@mail.ru>
2020-09-23 22:13:24 +0300

    Commit

--- role.courier.lvl3.js
+++ role.courier.lvl3.js
@@ -3,13 +3,19 @@ var roleCourierLvl3 = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.store.getUsedCapacity() !== 0 ){
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_EXTENSION ||
-                        structure.structureType === STRUCTURE_SPAWN ||
-                        structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
+            var target = undefined;
+            if(creep.memory.target) {
+                target = Game.getObjectById(creep.memory.target);
+            }
+            if(!target) {
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType === STRUCTURE_EXTENSION ||
+                            structure.structureType === STRUCTURE_SPAWN ||
+                            structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+                });
+            }
             if(!target) {
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => structure.structureType === STRUCTURE_CONTAINER
@@ -47,12 +53,16 @@ var roleCourierLvl3 = {
                 }
             }
             if(target) {
+                creep.memory.target = target.id;
                 if(creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
+                } else {
+                    creep.memory.target = undefined;
                 }
             }
             else {
                 creep.moveTo(Game.flags.IdleFlag);
+                creep.memory.target = undefined;
             }
         }
         else {
@@ -63,29 +73,38 @@ var roleCourierLvl3 = {
             //         && structure.store.getUsedCapacity() >= creep.store.getFreeCapacity()
             // });
             var target = undefined;
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            if(creep.memory.target) {
+                target = Game.getObjectById(creep.memory.target);
+            }
+            if(!target) {
+                var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => structure.structureType === STRUCTURE_CONTAINER
                         && Memory.structures['id'+structure.id]
                         && Memory.structures['id'+structure.id].containerType === 'Harvest'
                         && structure.store.getUsedCapacity() >= creep.store.getFreeCapacity()
-            });
-            if(targets && targets.length > 0) {
-                //target = _.minBy(targets, (e) => e.store.getUsedCapacity())
-                let max = -1;
-                for(let i in targets) {
-                    if (targets[i].store.getUsedCapacity() > max) {
-                        max = targets[i].store.getUsedCapacity();
-                        target = targets[i];
+                });
+                if(targets && targets.length > 0) {
+                    //target = _.minBy(targets, (e) => e.store.getUsedCapacity())
+                    let max = -1;
+                    for(let i in targets) {
+                        if (targets[i].store.getUsedCapacity() > max) {
+                            max = targets[i].store.getUsedCapacity();
+                            target = targets[i];
+                        }
                     }
                 }
             }
             if(target) {
+                creep.memory.target = target.id;
                 if(creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
+                } else {
+                    creep.memory.target = undefined;
                 }
             }
             else {
                 creep.moveTo(Game.flags.IdleFlag);
+                creep.memory.target = undefined;
             }
         }
     }
