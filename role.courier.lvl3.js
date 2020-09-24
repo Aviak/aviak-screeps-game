4a42559867ee6fe585bae95bb23579c109bafc32
Aviak <aviack@mail.ru>
2020-09-24 22:31:01 +0300

    Commit

--- role.courier.lvl3.js
+++ role.courier.lvl3.js
@@ -100,7 +100,7 @@ var roleCourierLvl3 = {
             if(target) {
                 creep.memory.target = target.id;
                 if(creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    if(creep.ticksToLive < 2 && !Memory['onDeath'+creep.id]) {
+                    if(creep.ticksToLive < 3 && !Memory['onDeath'+creep.id]) {
                         if(creep.memory.requested && creep.memory.requested > 0 && Memory.structures['id'+target.id].requested) {
                             Memory.structures['id'+target.id].requested -= creep.memory.requested;
                             creep.memory.requested = 0;
