55d6d1af47f8c60ab9ce398ca818a4b64b69119c
Aviak <aviack@mail.ru>
2020-09-24 17:42:29 +0300

    Commit

--- InvasionControl.js
+++ InvasionControl.js
@@ -10,6 +10,7 @@ var invasionControl = {
         let all_ranged_v1 = _.filter(Game.creeps, (creep) => creep.memory.role === 'ranged_v1' && creep.memory.gang);
         for(let creepName in all_ranged_v1) {
             let creep = all_ranged_v1[creepName];
+            console.log('#'+creep.gang);
             if(!Memory.invasionParameters.gangs[creep.gang].creeps.contains(creep.id)) {
                 creep.memory.gang = undefined;
             }
