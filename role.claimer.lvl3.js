915ea5d694b51013138561d0ee14c11022293d64
Aviak <aviack@mail.ru>
2020-09-24 18:57:10 +0300

    Commit

--- role.claimer.lvl3.js
+++ role.claimer.lvl3.js
@@ -26,22 +26,22 @@ var roleLongDistanceMinerLvl3 = {
             // creep.memory.longDistanceMining.x = location.x;
             // creep.memory.longDistanceMining.y = location.y;
 
-            console.log('2222');
-            if (creep.room.name !== roomName) {
-                console.log('1111');
-                let exitCode = creep.room.findExitTo(roomName);
-                let exitPos = creep.pos.findClosestByPath(exitCode, {ignoreCreeps: true});
-                //console.log('INIT MINER');
-                if (!exitPos) {
-                    //wait a turn
-                    //console.log('WAIT A TURN');
-                    console.log('3333');
-                    creep.memory.claiming = undefined;
-                    return;
-                }
-                creep.memory.claiming.exitToRoom = { x: exitPos.x, y: exitPos.y};
-            }
+            //console.log('2222');
 
+        }
+        if (creep.room.name !== roomName) {
+           // console.log('1111');
+            let exitCode = creep.room.findExitTo(roomName);
+            let exitPos = creep.pos.findClosestByPath(exitCode, {ignoreCreeps: true});
+            //console.log('INIT MINER');
+            if (!exitPos) {
+                //wait a turn
+                //console.log('WAIT A TURN');
+                //console.log('3333');
+                creep.memory.claiming = undefined;
+                return;
+            }
+            creep.memory.claiming.exitToRoom = { x: exitPos.x, y: exitPos.y};
         }
 
 
