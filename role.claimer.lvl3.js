817ec1a9464a170e573572177bdbdc941b6c8e0e
Aviak <aviack@mail.ru>
2020-09-24 18:56:34 +0300

    Commit

--- role.claimer.lvl3.js
+++ role.claimer.lvl3.js
@@ -26,13 +26,16 @@ var roleLongDistanceMinerLvl3 = {
             // creep.memory.longDistanceMining.x = location.x;
             // creep.memory.longDistanceMining.y = location.y;
 
+            console.log('2222');
             if (creep.room.name !== roomName) {
+                console.log('1111');
                 let exitCode = creep.room.findExitTo(roomName);
                 let exitPos = creep.pos.findClosestByPath(exitCode, {ignoreCreeps: true});
                 //console.log('INIT MINER');
                 if (!exitPos) {
                     //wait a turn
                     //console.log('WAIT A TURN');
+                    console.log('3333');
                     creep.memory.claiming = undefined;
                     return;
                 }
