29e7b67ba902a39a43796d1571e27961fa019bad
Aviak <aviack@mail.ru>
2020-09-28 13:16:11 +0300

    Commit

--- main.js
+++ main.js
@@ -546,7 +546,7 @@ function RunLatest() {
                 let damagedCreeps = towers[tower].room.find(FIND_MY_CREEPS, {
                     filter : (creep) => creep.hits < creep.hitsMax
                 });
-                console.log('en ' + tower.energy);
+                console.log('en ' + JSON.stringify(tower.store));
                 if(damagedCreeps && damagedCreeps.length > 0) {
                     towers[tower].heal(damagedCreeps[0]);
                 }
